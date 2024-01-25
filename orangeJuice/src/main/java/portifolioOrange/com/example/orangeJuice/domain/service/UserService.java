b9671984094dd05fc13ebc27f7f095f7236a43dd
Carla Ribeiro <carlaribeiro.ads@gmail.com>
2024-01-25 18:58:44 -0300

    bug: ajuste

--- orangeJuice/src/main/java/portifolioOrange/com/example/orangeJuice/domain/service/UserService.java
+++ orangeJuice/src/main/java/portifolioOrange/com/example/orangeJuice/domain/service/UserService.java
@@ -1,19 +1,19 @@
 package portifolioOrange.com.example.orangeJuice.domain.service;
 
 
-import portifolioOrange.com.example.orangeJuice.domain.entity.Project;
+import portifolioOrange.com.example.orangeJuice.domain.entity.User;
 
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface ProjectService {
-    List<Project> searchAll();
+public interface UserService {
+    List<User> searchAll();
 
     Optional<Project> searchById(UUID id);
 
-    User create(Project project);
+    User create(User user);
 
     User update(UUID id, Map<String, Object> params);
 
