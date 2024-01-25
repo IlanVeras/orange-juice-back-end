e378032c1cecdcc0e52ee62af712b1738ac1d147
Carla Ribeiro <carlaribeiro.ads@gmail.com>
2024-01-25 13:59:09 -0300

    feat: ProjectServiceImpl

---
+++ orangeJuice/src/main/java/portifolioOrange/com/example/orangeJuice/domain/entity/ImgProject.java
@@ -0,0 +1,22 @@
+package portifolioOrange.com.example.orangeJuice.domain.entity;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(project= "project")
+@Table(project = "projects")
+@JsonIgnoreProperties
+
+@Entity
+public class ImgProject
