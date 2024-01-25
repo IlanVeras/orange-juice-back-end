3f2f6fdb30223260a9d11e490f9ae2047b22df55
Carla Ribeiro <carlaribeiro.ads@gmail.com>
2024-01-25 14:13:57 -0300

    feat: entidade (ImgProject )

--- orangeJuice/src/main/java/portifolioOrange/com/example/orangeJuice/domain/entity/ImgProject.java
+++ orangeJuice/src/main/java/portifolioOrange/com/example/orangeJuice/domain/entity/ImgProject.java
@@ -7,16 +7,53 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.util.UUID;
+import java.util.Base64;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonProperty.Access;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-@Entity(project= "project")
-@Table(project = "projects")
+@Entity(ImgProject= "ImgProject")
+@Table(ImgProject = "ImgProject")
 @JsonIgnoreProperties
+public class ImgProject{
 
-@Entity
-public class ImgProject
+    @Id
+    @GeneratedValue(Strategy = GeneretionType.Identity)
+    private Integer id;
+
+    @JsonProperty(acess = Acess.WRITE_ONLY)
+    @Lob
+    @Column 
+    private byte[] dados;
+    
+    @Column 
+    private String nome;
+
+
+    public Integer getId(){
+        return id;
+    }
+    
+    public void setId(Integer Id){
+        this.id = id;
+    }
+    
+    public byte[] getDados(){
+        return dados;
+    }
+    public void setDados( String dados){
+        this.dados = Base64.getDecoder().decode(dados);
+        }
+
+    }
+
+    public String getNome(){
+        return nome;
+    }
+
+public void setNome(String nome){
+    this.nome = nome;
+}
